e7f04b38833ac322f56070c5d035ee186e3482b4
Gnomi2 <gnomi2@valur.local>
2026-02-17 21:13:39 +0000

    Remove duplicate navigation from HubShell

--- src/components/shell/HubShell.tsx
+++ src/components/shell/HubShell.tsx
@@ -1,11 +1,7 @@
 "use client";
 import React from 'react';
-
-import OrientationHeader from "@/components/zones/OrientationHeader";
-import NavigationRail from "@/components/shell/NavigationRail";
 import FluidStage from "@/components/zones/FluidStage";
 import InputDeck from "@/components/zones/InputDeck";
-
 import { useEffect } from "react";
 import { useFamilyHub } from "@/lib/contexts/FamilyHubContext";
 
@@ -18,33 +14,18 @@ export default function HubShell() {
 
     return (
         <div className="flex flex-col h-screen overflow-hidden transition-colors duration-500 bg-[var(--background-dark)]">
-            {/* Zone A: Top Orientation Header */}
-            <div className="flex-none z-50">
-                <OrientationHeader />
-            </div>
+            {/* Main Content Area: FluidStage + Input */}
+            <main className="flex flex-col flex-1 relative min-w-0">
+                {/* Zone B: Fluid Stage */}
+                <div className="flex-1 overflow-hidden relative">
+                    <FluidStage />
+                </div>
 
-            {/* Main Layout Row */}
-            <div className="flex flex-1 overflow-hidden relative">
-                {/* Left Navigation Rail */}
-                <aside className="w-[80px] md:w-[100px] flex-none z-40 bg-[var(--surface-dark)] border-r border-[var(--border)]">
-                    <NavigationRail />
-                </aside>
-
-                {/* Right Content Area: FluidStage + Input */}
-                <main className="flex flex-col flex-1 relative min-w-0">
-                    
-                    {/* Zone B: Fluid Stage (takes available space) */}
-                    <div className="flex-1 overflow-hidden relative">
-                        <FluidStage />
-                    </div>
-
-                    {/* Zone C: Input Deck (bottom anchor) */}
-                    <div className="flex-none z-30 pb-4 px-4 md:px-8">
-                        <InputDeck />
-                    </div>
-
-                </main>
-            </div>
+                {/* Zone C: Input Deck */}
+                <div className="flex-none z-30 pb-4 px-4 md:px-8">
+                    <InputDeck />
+                </div>
+            </main>
         </div>
     );
 }
