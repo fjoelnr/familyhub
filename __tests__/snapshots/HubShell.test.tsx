c6b3ea7d4e5b73aac0e53fd03c40677f2cb85552
fjoelnir <thrudhamar@web.de>
2026-03-20 12:30:08 +0100

    test: stabilize hub shell snapshot

--- __tests__/snapshots/HubShell.test.tsx
+++ __tests__/snapshots/HubShell.test.tsx
@@ -5,6 +5,14 @@ import HubShell from '@/components/shell/HubShell';
 import { FamilyHubProvider } from '@/lib/contexts/FamilyHubContext';
 import { AgentRuntimeProvider } from '@/lib/contexts/AgentRuntimeContext';
 
+jest.mock('@/components/zones/FluidStage', () => function MockFluidStage() {
+    return <div data-testid="fluid-stage">FluidStage</div>;
+});
+
+jest.mock('@/components/zones/InputDeck', () => function MockInputDeck() {
+    return <div data-testid="input-deck">InputDeck</div>;
+});
+
 describe('HubShell Snapshot', () => {
     beforeAll(() => {
         jest.useFakeTimers();
